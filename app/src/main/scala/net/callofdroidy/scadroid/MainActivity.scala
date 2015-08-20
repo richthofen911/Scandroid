6131b2021714e995f5553103e81653f454dfaa12
richthofen911 <yichaoli.richthofen@gmail.com>
2015-08-19 22:42:46 -0400

    solve multidex issue

--- app/src/main/scala/net/callofdroidy/scadroid/MainActivity.scala
+++ app/src/main/scala/net/callofdroidy/scadroid/MainActivity.scala
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.{View, Menu}
 import android.view.View.OnClickListener
 import android.widget.TextView
+import com.android.volley.Response.{ErrorListener, Listener}
+import com.android.volley.{VolleyError, Response, Request}
+import com.android.volley.toolbox.{Volley, StringRequest}
 ;
 
 import scala.Unit;
@@ -19,11 +22,19 @@ class MainActivity extends AppCompatActivity {
 
         val tv_display = findViewById(R.id.tv_display).asInstanceOf[TextView]
 
+        val requestTest = new StringRequest(Request.Method.GET, "https://www.google.ca/?gws_rd=ssl", new Response.Listener[String](){
+            override def onResponse(response: String) = tv_display.setText(response)
+        }, new ErrorListener {
+            override def onErrorResponse(error: VolleyError) = tv_display.setText(error.toString)
+        })
+
         findViewById(R.id.btn_change).setOnClickListener(new OnClickListener {
             override def onClick(v: View) = {
-                tv_display.setText("Hi Scala")
+                Volley.newRequestQueue(getApplicationContext).add(requestTest)
             }
         })
+
+
 
 
     }
